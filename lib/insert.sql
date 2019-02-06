3df5cc77e805df5a15b78e70a5df38e85609009a
camfeghali <camfeg@gmail.com>
2019-02-06 11:28:05 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'male', 'brown', 'pissed', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Narvouz', 4, 'male', 'brown', 'content', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Hariri', 6, 'female', 'blue', 'pissed', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Jemblat', 8, 'female', 'brown', 'happe', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Aoun', 12, 'male', 'orange', 'frustrated', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Geagea', 15, 'female', 'red', 'pissed', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Berri', 38, 'male', 'yellow', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 14, 'male', 'green', 'pissed', 0);
